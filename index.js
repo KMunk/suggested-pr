a5b03c15df0f7cdc68687cb79b701fdbe89b2132
KMunk <kmunk@cloud-cme.com>
2021-09-15 12:35:20 -0400

    async function

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try{
-    const githubToken = core.getInput('GITHUB_TOKEN');
-    const owner = core.getInput('owner');
-    const repo = core.getInput('repo');
-    const context = github.context;
+async function run(){
+    try{
+        const githubToken = core.getInput('GITHUB_TOKEN');
+        const owner = core.getInput('owner');
+        const repo = core.getInput('repo');
+        const context = github.context;
+        
+        if (context.payload.pull_request == null) {
+          core.setFailed('No pull request found.');
+          return;
+        }
+        const pullRequestNumber = context.payload.pull_request.number;
+        const headRef = `${owner}:${context.payload.pull_request.head.ref}`
     
-    if (context.payload.pull_request == null) {
-      core.setFailed('No pull request found.');
-      return;
-    }
-    const pullRequestNumber = context.payload.pull_request.number;
-    const headRef = `${owner}:${context.payload.pull_request.head.ref}`
-
-    const octokit = new github.getOctokit(githubToken);
-    
-    const { data: pullRequests } = octokit.rest.pulls.list({
-        owner: owner,
-        repo: repo,
-        state: 'open',
-        head: headRef
-    });
-
-    if(pullRequests != null){
-        let message = "Possible Related Pull Requests:<br/>"
-        pullRequests.forEach((pr) =>{
-            message += `${pr.url}<br/>`
+        const octokit = new github.getOctokit(githubToken);
+        
+        const { data: pullRequests } = octokit.rest.pulls.list({
+            owner: owner,
+            repo: repo,
+            state: 'open',
+            head: headRef
         });
-
-        octokit.issues.createComment({
-            ...context.repo,
-            issue_number: pullRequestNumber,
-            body: message
-          });
+    
+        if(pullRequests != null){
+            let message = "Possible Related Pull Requests:<br/>"
+            pullRequests.forEach((pr) =>{
+                message += `${pr.url}<br/>`
+            });
+    
+            octokit.issues.createComment({
+                ...context.repo,
+                issue_number: pullRequestNumber,
+                body: message
+              });
+        }
+    
+    } catch (error) {
+        core.setFailed(error.message);
     }
-
-} catch (error) {
-    core.setFailed(error.message);
 }
+
+run();
